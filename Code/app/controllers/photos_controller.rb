8a70689cb47a45a7392dc1d4c45f36b4d1bc505b
Christos Gkovaris <christosgkovariscs52@gmail.com>
2024-12-15 19:10:31 +0200

    Add files via upload

--- Code/app/controllers/photos_controller.rb
+++ Code/app/controllers/photos_controller.rb
@@ -1,43 +1,56 @@
 class PhotosController < ApplicationController
 
-  def destroy
-    @photo = Photo.find(params[:id])
-
-    if current_user == @photo.user
-      @photo.destroy
-      flash[:notice] = "Photo and all related comments and tags were successfully deleted!"
-    else
-      flash[:alert] = "You are not authorized to delete this photo."
-    end
-    redirect_to user_path(current_user)
-  end
-
-  def show
-    @photo = Photo.find(params[:id])
-  end
-
-  def create
-    if params[:photo].nil?
-      flash[:alert] = "Please upload a photo"
-      redirect_to :back
-    else
-      @photo = current_user.photos.new(photo_params)
-      if @photo.save
-        flash[:notice] = "Successfully uploaded a photo"
+    def destroy
+      @photo = Photo.find(params[:id])
+  
+      if current_user == @photo.user
+        @photo.destroy
+        flash[:notice] = "Photo and all related comments and tags were successfully deleted!"
       else
-        flash[:alert] = "Failed to upload photo. Please try again."
+        flash[:alert] = "You are not authorized to delete this photo."
       end
       redirect_to user_path(current_user)
     end
-  end
+    
+    def comments
+      @photo = Photo.find(params[:id])
+      @comments = @photo.comments.includes(:user).order(created_at: :desc)
 
-  def new
-    @photo = Photo.new
-  end
+      render json: @comments.as_json(include: { user: { only: :email } }) 
+    end
 
-  private
-
-  def photo_params
-    params.require(:photo).permit(:image, :title)
-  end
-end
+    def show
+      @photo = Photo.find(params[:id])
+      @followed_users_photos = current_user.followed_users.includes(:photos).map do |user|
+        {
+          user: user,
+          photos: user.photos.order(created_at: :desc)
+        }
+      end
+    end
+  
+    def create
+      if params[:photo].nil?
+        flash[:alert] = "Please upload a photo"
+        redirect_to :back
+      else
+        @photo = current_user.photos.new(photo_params)
+        if @photo.save
+          flash[:notice] = "Successfully uploaded a photo"
+        else
+          flash[:alert] = "Failed to upload photo. Please try again."
+        end
+        redirect_to user_path(current_user)
+      end
+    end
+  
+    def new
+      @photo = Photo.new
+    end
+  
+    private
+  
+    def photo_params
+      params.require(:photo).permit(:image, :title)
+    end
+  end  
